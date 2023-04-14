3ef5efd5905d3eb8b09e105a24f8cb98dc6c589c
zrthstr <zrth1k@gmail.com>
2023-04-14 20:40:33 +0200

    cleanup gdt64

--- v6-tbd/kernel.c
+++ v6-tbd/kernel.c
@@ -25,12 +25,14 @@ void screen_clear(void);
 void screen_paint(int);
 void set_up_paging(void);
 //void cpuid(void);
-void cpu_id(const char *cpuname);
+int cpu_id(const char *cpuname);
 unsigned int cpu_lm_bit(void);
 int cpu_chk_interups(void);
 int get_cr0(void);
 void int_to_bin_str(int, char *);
 void int_to_hex_str(int, char *);
+void write_gdt64(void);
+
 
 void main(void){
 
@@ -105,14 +107,14 @@ int get_cr0(){
 	__asm__ volatile(
 		"mov cr0, eax":"=a"(cr0));
 	return cr0;
-};
+}
 
 
 void int_to_bin_str(int n, char * str){
 	for (int i = 0; i < 32; i++){
 		str[i] = ((n >> (31 - i)) & 1 ) + '0';
 	}
-};
+}
 
 void int_to_hex_str(int n, char * str){
 	char map[] = "0123456789abcdef";
@@ -123,7 +125,7 @@ void int_to_hex_str(int n, char * str){
 		str[i] = map[ix];
 	}
 	str[i] = '\0'; 
-};
+}
 
 int cpu_chk_interups(void){
 	unsigned int flags;
@@ -133,7 +135,7 @@ int cpu_chk_interups(void){
 	return flags & (1 << 9);
 }
 
-cpu_id(const char * cpuname) {
+int cpu_id(const char * cpuname) {
 	// get the cpu vendor given name
 	//cpuid_string(0, (int*)(cpuname));
 
@@ -162,8 +164,116 @@ unsigned int cpu_lm_bit(void){
 	return mode;
 }
 
+void write_gdt64(){
+
+	/// lets stuff the gtd64 intto 0x500
+
+	//lgdt [gdt64.pointer]
+	//jmp gdt64.code:longstart
+	//
+	//gdt64:
+	//    dq 0
+	//.code: equ $ - gdt64
+	//    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
+	//.pointer:
+	//    dw $ - gdt64 - 1 ; length of the gdt64 table
+	//    dq gdt64         ; addess of the gdt64 table
+	//
+
+	// 3x dq + dw 
+	// dq -> 8 byte
+	// dw -> 2 bytes
+	// => 26?!?
+	
+	// uint64_t mask = (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53);
+	uint8_t *gdt64 = (uint8_t *) 0x500;
+	//gdt64[8] = mask; // WWONT ORK
+	//uint64_t *gdt64_8 = (uint64_t *) 0x508;
+
+
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+
+	*gdt64++ = 0x66;
+	*gdt64++ = 0x0F;
+	*gdt64++ = 0x01;
+	*gdt64++ = 0x0D;
+
+	*gdt64++ = 0xFB;
+	*gdt64++ = 0x02;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+
+	*gdt64++ = 0x48;
+	*gdt64++ = 0x8D;
+	*gdt64++ = 0x1D;
+	*gdt64++ = 0xF4;
+
+	*gdt64++ = 0x02;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+	*gdt64++ = 0x00;
+
+	// 0x500 - 0x507 |  00 00 00 00 00 00 00 00  ; gdt64: dq 0
+	// 0x508 - 0x50F |  66 0F 01 0D FB 02 00 00  ; .pointer: dw $ - gdt64 - 1 ; length of the gdt64 table
+	// 0x510 - 0x517 |  48 8D 1D F4 02 00 00     ;          dq gdt64         ; address of the gdt64 table
+
+	//gdt64[16] = 26; // words??
+	//gdt64[16] = 52; // bytes!?!?
+	
+
+	/// ▷⋅⋅⋅//    dq gdt64         ; addess of the gdt64 table 
+	//gdt64[17] = "0x5";
+	//gdt64[18] = 0x0;
+	
+}
+
+void write_mini_64bit_kernel(){
+	// 0x00007E00
+	uint8_t *buffer = (uint8_t*)0x7E00;
+
+	// 00000000: 66c7 0425 0080 0b00 4f0e 66c7 0425 0280  f..%....O.f..%..
+	// 00000010: 0b00 4b0e f4   
+
+	*buffer++ = 0x66 ;
+	*buffer++ = 0xc7 ;
+	*buffer++ = 0x04 ;
+	*buffer++ = 0x25 ;
+
+	*buffer++ = 0x00 ;
+	*buffer++ = 0x80 ;
+	*buffer++ = 0x0b ;
+	*buffer++ = 0x00 ;
+
+	*buffer++ = 0x4f ;
+	*buffer++ = 0x0e ;
+	*buffer++ = 0x66 ;
+	*buffer++ = 0xc7 ;
+
+	*buffer++ = 0x04 ;
+	*buffer++ = 0x25 ;
+	*buffer++ = 0x02 ;
+	*buffer++ = 0x80 ;
+
+	*buffer++ = 0x0b ;
+	*buffer++ = 0x00 ;
+	*buffer++ = 0x4b ;
+	*buffer++ = 0x0e ;
+
+	*buffer++ = 0xf4 ;
+}
+
 void set_up_paging(){
 // https://github.com/starsheriff/train-os/tree/master/06-switch-to-long-mode
+	write_mini_64bit_kernel();
+	write_gdt64(); // to 0x500
 
 /*
 	; Step 2: Enable Physical Address Extension
@@ -171,28 +281,33 @@ void set_up_paging(){
 	or eax, (1 << 5)
 	mov cr4, eax
 */
-  /// tested, woks.. (creg -> CR4=0x00000020)
+	/// tested, woks.. (creg -> CR4=0x00000020)
 	__asm__ volatile (
 		"mov eax, cr4 \n"
 		"or eax, (1 << 5 ) \n"
 		"mov cr4, eax \n");
 
 
-	//uint8_t * buffer = (uint8_t *) 0x10000;
-	//char *buffer = (char*)0x10000;
-	
 	// this must align to 4096
 	uint8_t *buffer = (uint8_t*)0x10000;
 
-	/// TODO: lets null this just to be sure
+	// lets null this just to be sure
 	uint32_t buffer_len = 4096 * 3;
 	for (int i = 0; i < buffer_len; i ++){
 		buffer[i] = 0x00;
 	}
 
-	uint8_t *p4_table = (uint8_t*) 0x10000;
-	uint8_t *p3_table = (uint8_t*) 0x11000;
-	uint8_t *p2_table = (uint8_t*) 0x12000;
+	// lets also null this
+	uint32_t * gbuffer = 0x500;
+	for (int i = 0; i < 100; i ++){
+		gbuffer[i] = 0x00;
+	}
+
+
+	// out page tables. 4096 byte each..
+	uint32_t *p4_table = (uint32_t*) 0x10000;
+	uint32_t *p3_table = (uint32_t*) 0x11000;
+	uint32_t *p2_table = (uint32_t*) 0x12000;
 
 	/*
 	*	; Step 3: Set `cr3` register
@@ -200,54 +315,36 @@ void set_up_paging(){
 	*	mov cr3, eax
 	*/
 	// -> CR3=0x000000010000
-	int cr3 = p4_table;
 	__asm__ volatile (
 		"mov eax, %0  \n"
 		"mov cr3, eax \n"
-		: : "r"(cr3));
+		: : "r"(p4_table));
 
-	//PDE_PRESENT  equ 1 << 0
-	//PDE_WRITABLE equ 1 << 1
-	//PDE_LARGE    equ 1 << 7
 
-    //  mov eax, (0x20_0000 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE)
-	//  mov [p2_table + 8], eax
+	// ; Step 4: Set the p2[1] entry to point to the _second_ 2 MiB frame
+	// mov eax, (0x20_0000 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE)
+	// mov [p2_table + 8], eax
+	//
+	// ; point the 0th entry to the first frame
+	// mov eax, (0x00_0000 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE)
+	// mov [p2_table], eax
 
-	uint32_t PDE_PRESENT = 1 << 0;
+
+	uint32_t PDE_PRESENT  = 1 << 0;
 	uint32_t PDE_WRITABLE = 1 << 1;
-	uint32_t PDE_LARGE = 1 << 7;
+	uint32_t PDE_LARGE    = 1 << 7;
 
-	uint32_t v_block_0 = 0x000000;
-	uint32_t v_block_1 = 0x200000;
-	uint32_t p2_table_mask_1 = (v_block_1 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE);
+	*p2_table = (0x000000 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE);  //    0x83
 
-	// 0x0000000000012000 <bogus+       0>:	0x00000000	0x00000000	0x00200083
-	// -> '0b1000000000000010000011'
-	__asm__ volatile (
-		"mov eax, %0\n"
-		"mov [%1 + 8], eax\n"
-		:
-		:"r"(p2_table_mask_1), "r"(p2_table)
-		:"eax");
+	*(p2_table + 8) = (0x200000 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE);  // 0x20183
 
-	uint32_t p2_table_mask_0 = (v_block_0 | PDE_PRESENT | PDE_WRITABLE | PDE_LARGE);
-	__asm__ volatile (
-		"mov eax, %0\n"
-		"mov [%1], eax\n"
-		:
-		:"r"(p2_table_mask_0), "r"(p2_table)
-		:"eax");
 
 	//; Step 5: Set the 0th entry of p3 to point to our p2 table
 	//mov eax, p2_table ; load the address of the p2 table
 	//or eax, (PDE_PRESENT | PDE_WRITABLE)
 	//mov [p3_table], eax
 
-	uint32_t p3_0 = ( (uint32_t)  p2_table | PDE_PRESENT | PDE_WRITABLE);
-	__asm__ volatile (
-		"mov eax, %0\n"
-		"mov [%1], eax\n"
-		::"p"(p3_0), "p"(p3_table));
+	*p3_table = ((uint32_t)p2_table | PDE_PRESENT | PDE_WRITABLE);
 
 
 	//; Step 6: Set the 0th entry of p4 to point to our p3 table
@@ -255,11 +352,7 @@ void set_up_paging(){
 	//or eax, (PDE_PRESENT | PDE_WRITABLE)
 	//mov [p4_table], eax
 
-	uint32_t p4_0 = ((uint32_t) p3_table | PDE_PRESENT | PDE_WRITABLE);
-	__asm__ volatile (
-		"mov eax, %0\n"
-		"mov [%1], eax\n"
-		::"p"(p4_0), "p"(p4_table));
+	*p4_table = ((uint32_t)p3_table | PDE_PRESENT | PDE_WRITABLE);
 
 
 	// ; Step 7: Set EFER.LME to 1 to enable the long mode
@@ -280,251 +373,27 @@ void set_up_paging(){
 	// mov eax, cr0
 	// or eax, 1 << 31
 	// mov cr0, eax
-	
+
 	__asm__ volatile (
 		"mov eax, cr0 \n"
 		"or eax, 1 << 31 \n"
 		"mov cr0, eax "
-		);
+	);
 
 
 	//lgdt [gdt64.pointer]
-	//jmp gdt64.code:longstart
-	//
-	//gdt64:
-	//    dq 0
-	//.code: equ $ - gdt64
-	//    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
-	//.pointer:
-	//    dw $ - gdt64 - 1 ; length of the gdt64 table
-	//    dq gdt64         ; addess of the gdt64 table
-	//
+	__asm__ volatile("lgdt [0x510] \n");
 
-	// 3x dq + dw 
-	// dq -> 8 byte
-	// dw -> 2 bytes
-	// => 26?!?
-	
-	uint64_t mask = (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53);
-	uint8_t *gdt64 = (uint8_t *) 0x500;
-	//gdt64[8] = mask; // WWONT ORK
-	uint64_t gdt64_8 = (uint64_t *) 0x508;
-	gdt64_8 = mask;
 
-	gdt64[16] = 26 // words??
-	gdt64[16] = 52 // bytes??
-	
+	// now we jump mem location 0x7E00
+	// is the segment 0 or 1?
+	// or is it code aka 8?
+	__asm__ volatile ("jmp 8:0x7E00 \n");
 
-	ONLY THIS LEFT
-	/// ▷⋅⋅⋅//    dq gdt64         ; addess of the gdt64 table 
-	gdt64_8[]... 
 
+	/// We dont need a stack, no?
 }
 
-void set_up_paging_copy_new_finaldotexedotdoceleven(){
-
-	
-	///// https://wiki.osdev.org/Setting_Up_Long_Mode
-
-	/* 
-	 * start by nulling the memory where we want out page tables to go
-	 *
-	 *   mov edi, 0x1000    ; Set the destination index to 0x1000.
-	 *   mov cr3, edi       ; Set control register 3 to the destination index.
-	 *   xor eax, eax       ; Nullify the A-register.
-	 *   mov ecx, 4096      ; Set the C-register to 4096.
-	 *   rep stosd          ; Clear the memory.
-	 *   mov edi, cr3       ; Set the destination index to control register 3.
-	 *
-	 */
-	uint8_t* buffer = (uint8_t*) 0x1000;  // Pointer to buffer starting at 0x1000
-	// Clear the buffer by setting each byte to zero
-	for (size_t i = 0; i < 4096; i++) {
-		buffer[i] = 0;
-	}
-
-	/* 
-	 *  mov DWORD [edi], 0x2003      ; Set the uint32_t at the destination index to 0x2003.
-	    add edi, 0x1000              ; Add 0x1000 to the destination index.
-	    mov DWORD [edi], 0x3003      ; Set the uint32_t at the destination index to 0x3003.
-	    add edi, 0x1000              ; Add 0x1000 to the destination index.
-	    mov DWORD [edi], 0x4003      ; Set the uint32_t at the destination index to 0x4003.
-	    add edi, 0x1000              ; Add 0x1000 to the destination index.
-	
-noticed: we used a three. This set the first 2 bit.
-These bits indicate that the page is present, readable & writable
-*/
-
-
-	//int edi = 0x1000;
-	int edi = 0x0000;
-	buffer[edi] = 0x2003;
-	edi += 0x1000;
-	buffer[edi] = 0x3003;
-	edi += 0x1000;
-	buffer[edi] = 0x4003;
-	edi += 0x1000;
-
-/*  Now lets identity map the first two megabytes:
-	    mov ebx, 0x00000003          ; Set the B-register to 0x00000003.
-	    mov ecx, 512                 ; Set the C-register to 512.
-	
-	.SetEntry:
-	    mov DWORD [edi], ebx         ; Set the uint32_t at the destination index to the B-register.
-	    add ebx, 0x1000              ; Add 0x1000 to the B-register.
-	    add edi, 8                   ; Add eight to the destination index.
-	    loop .SetEntry               ; Set the next entry.
-	
-	 */
-
-// Now lets identity map the first two megabytes, in c
-	int ebx = 0x00000003;
-	int ecx = 512;
-
-	// is this off by one? and would this matter?
-	while (ecx > 0){
-	//mov DWORD [edi], ebx         ; Set the uint32_t at the destination index to the B-register.
-	buffer[edi] = ebx; // ??
-	ebx += 0x1000;
-	edi += 8;
-	ecx--;
-	}
-
-		__asm__ volatile (
-		"mov eax, cr4   \n"    // ; Set the A-register to control register 4.
-		"or eax, 1 << 5 \n"    // ; Set the PAE-bit, which is the 6th bit (bit 5).
-		"mov cr4, eax   \n"    // ; Set control register 4 to the A-register.
-	);
-
-	__asm__ volatile (
-		"mov ecx, 0xC0000080  \n"    // ; Set the C-register to 0xC0000080, which is the EFER MSR.
-		"rdmsr                \n"    // ; Read from the model-specific register.
-		"or eax, 1 << 8       \n"    // ; Set the LM-bit which is the 9th bit (bit 8).
-		"wrmsr                \n"    // ; Write to the model-specific register.
-	);
-
-	__asm__ volatile (
-		"mov eax, cr0       \n"      //  ; Set the A-register to control register 0.
-		"or eax, 1 << 31    \n"      //  ; Set the PG-bit, which is the 32nd bit (bit 31).
-		"mov cr0, eax       \n"      //  ; Set control register 0 to the A-register.
-		);
-
-
-	// now set up a nice and propper 64 bit gtd
-	/*
-	Access bits
-	PRESENT        equ 1 << 7
-	NOT_SYS        equ 1 << 4
-	EXEC           equ 1 << 3
-	DC             equ 1 << 2
-	RW             equ 1 << 1
-	ACCESSED       equ 1 << 0
-
-	; Flags bits
-	GRAN_4K       equ 1 << 7
-	SZ_32         equ 1 << 6
-	LONG_MODE     equ 1 << 5
-
-	GDT:
-	  .Null: equ $ - GDT
-	       dq 0
-	    .Code: equ $ - GDT
-	        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
-	        db 0                                        ; Base (mid, bits 16-23)
-	        db PRESENT | NOT_SYS | EXEC | RW            ; Access
-	        db GRAN_4K | LONG_MODE | 0xF                ; Flags & Limit (high, bits 16-19)
-	        db 0                                        ; Base (high, bits 24-31)
-	    .Data: equ $ - GDT
-	        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
-	        db 0                                        ; Base (mid, bits 16-23)
-	        db PRESENT | NOT_SYS | RW                   ; Access
-	        db GRAN_4K | SZ_32 | 0xF                    ; Flags & Limit (high, bits 16-19)
-	        db 0                                        ; Base (high, bits 24-31)
-	    .TSS: equ $ - GDT
-	        dd 0x00000068
-	        dd 0x00CF8900
-	    .Pointer:
-	        dw $ - GDT - 1
-	        dq GDT
-		*/
-
-
-	/// GPT4: Here is a C bitwise equivalent of the given assembly code
-
-#define PRESENT   (1 << 7)
-#define NOT_SYS   (1 << 4)
-#define EXEC      (1 << 3)
-#define DC        (1 << 2)
-#define RW        (1 << 1)
-#define ACCESSED  (1 << 0)
-
-// Flags bits
-#define GRAN_4K   (1 << 7)
-#define SZ_32     (1 << 6)
-#define LONG_MODE (1 << 5)
-
-
-struct gdt_entry {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_mid;
-    uint8_t access;
-    uint8_t granularity;
-    uint8_t base_high;
-} __attribute__((packed));
-
-struct gdt_ptr {
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed));
-
-struct gdt_entry gdt[3];
-struct gdt_ptr gp;
-
-void gdt_init(void) {
-    // NULL descriptor
-    gdt[0].limit_low = 0;
-    gdt[0].base_low = 0;
-    gdt[0].base_mid = 0;
-    gdt[0].access = 0;
-    gdt[0].granularity = 0;
-    gdt[0].base_high = 0;
-
-    // Code descriptor
-    gdt[1].limit_low = 0xFFFF;
-    gdt[1].base_low = 0;
-    gdt[1].base_mid = 0;
-    gdt[1].access = PRESENT | NOT_SYS | EXEC | RW;
-    gdt[1].granularity = GRAN_4K | LONG_MODE | 0xF;
-    gdt[1].base_high = 0;
-
-    // Data descriptor
-    gdt[2].limit_low = 0xFFFF;
-    gdt[2].base_low = 0;
-    gdt[2].base_mid = 0;
-    gdt[2].access = PRESENT | NOT_SYS | RW;
-    gdt[2].granularity = GRAN_4K | SZ_32 | 0xF;
-    gdt[2].base_high = 0;
-
-    gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
-    gp.base = (uint32_t) &gdt[0];
-
-    // Load the GDT
-    __asm__ volatile ("lgdt %0" : : "m"(gp));
-    // Reload all segment registers
-    //asm volatile("mov $0x10, %ax \n mov %ax, %ds \n mov %ax, %es \n mov %ax, %fs \n mov %ax, %gs \n ljmp $0x08, $next \n next:");
-	}
-
-	gdt_init();
-
-
-	__asm__ volatile (
-		//"lgdt [GDT64.Pointer]   \n"   //  ; Load the 64-bit global descriptor table.
-		//"jmp GDT64.Code:Realm64 \n"   //  ; Set the code segment and enter 64-bit long mode.
-		"jmp 0x12345 \n"   //  ; Set the code segment and enter 64-bit long mode.
-			);
-
-}
 
 int kprint(const char *string){
 	int x = 0;
