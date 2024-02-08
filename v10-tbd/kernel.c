d1ece7e3219e181a5ab4e90deba467f6cf091630
zrthstr <zrth1k@gmail.com>
2024-02-08 13:01:36 +0100

    ..

--- v10-tbd/kernel.c
+++ v10-tbd/kernel.c
@@ -7,6 +7,31 @@
 //#define cprint(x, y); kprint_yx(x, POS++, 0, y);
 
 // [0x000000000602] 0008:0000000000000602 (unk. ctxt): add byte ptr ds:[rax], al ; 0000
+
+struct idt_entry {
+	uint16_t base_lo;
+	uint16_t sel;
+	uint8_t ist;
+	uint8_t flags;
+	uint16_t base_mid;
+	uint32_t base_hi;
+	uint32_t reserved;
+} __attribute__((packed));
+
+struct idt_ptr {
+	uint16_t limit;
+	uint64_t base;
+} __attribute__((packed));
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+void handle_interrupt(void);
+void cause_div_problems(void);
+void idt_install(void);
+//void idt_load(struct idt_ptr* idtp);
+void idt_set_gate(uint8_t num, uint64_t base, uint16_t sel, uint8_t flags);
+
 void main(void){
 
 
@@ -39,7 +64,7 @@ void main(void){
 
 	cprint("apparently we are in long mode.... AND PAGING", YELLOW);
 
-	//init_heap();
+	init_heap();
 
 	void * foo = malloc(1);
 	void * fooo = malloc(15);
@@ -48,14 +73,17 @@ void main(void){
 
 	int iiiii  = 0;
 
-	//handle_interrupt();
+	//idt_install();
+
+	__asm__ volatile ("sti"); // set the interrupt flag
+
+	 //__asm__ volatile ("cli; hlt");
+
+	handle_interrupt();
 	//
-	//cause_div_problems();
+	cause_div_problems();
 	//
 	//extern void init_idt();
-//init_idt();
-	//cause_div_problems();
-
 	//cause_page_problems();
 	for(;;){
 		//kprint_yx(".", POS++, 0, D);
@@ -63,24 +91,52 @@ void main(void){
 	}
 }
 
-//interrupt.c
-//#include "vga.h"
+
+void idt_load(struct idt_ptr idtp){
+	__asm__ volatile("lidt (%0)"   : : "m"(idtp) : "memory");
+}
+
+
+void idt_set_gate(uint8_t num, uint64_t base, uint16_t sel, uint8_t flags) {
+	idt[num].base_lo = (base & 0xFFFF);
+	idt[num].base_mid = (base >> 16) & 0xFFFF;
+	idt[num].base_hi = (base >> 32) & 0xFFFFFFFF;
+	idt[num].sel = sel;
+	idt[num].ist = 0;
+	idt[num].flags = flags;
+	idt[num].reserved = 0;
+}
+
+void idt_install(void){
+	idtp.limit = (sizeof ( struct idt_entry ) * 256 ) -1;
+	idtp.base = (uint64_t) &idt;
+	for (int i = 0; i < 256; i++) {
+		//idt_set_gate(i, /* ISR address */, /* segment */, /* flags */);
+		idt_set_gate(i, 0x1111111, 0x08, 0x8e );
+	}
+
+	//idt_load(&idtp);
+	idt_load(idtp);
+}
+
+
 
 // handle_interrupt prints a generic message to the screen and does not return.
-void handle_interrupt() {
+void handle_interrupt(void) {
     // print red on black
     //char color_code = vga_color_code(VGA_COLOR_RED, VGA_COLOR_BLACK);
     //vga_print(color_code, "\nInterrupt handled!");
 	print("INTERUP KINDA HANDLED");
-    while(1) {};
 }
 
-void cause_div_problems(){
+void cause_div_problems(void){
 	int b = 0;
 	int a = 9 / b;
 }
-void cause_page_problems(){
-	char *page_fault = (char *) 0x400000;
-	// Assigning a value to the memory address causes a page fault.
-	*page_fault = 42;
-}
+
+//void cause_page_problems(void){
+//	char *page_fault = (char *) 0x400000;
+//	// Assigning a value to the memory address causes a page fault.
+//	*page_fault = 42;
+//}
+
